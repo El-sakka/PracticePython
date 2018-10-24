f8546d41bef174194ea7a2555e999f2418198bfd
Elsakka <mahmoudelsakka95@gmail.com>
2018-10-24 22:20:34 +0200

    lab1 practice python

---
+++ first.py
@@ -0,0 +1,28 @@
+# Practice
+# 1- Write a program to prompt a user to enter his name, age and print these information on
+# the console
+
+# name = input("Enter your Name : ")
+# age = int(input("Enter your Age : "))
+# print("your name is "+name +"\nYour age is "+str(age))
+
+# Write a program to prompt a user to enter 2 numbers and print the values of adding,
+# subtracting, multiplying and dividing these 2 numbers
+
+# x = int(input("Enter first Number "))
+# y = int(input("Enter second Number "))
+# print("adding : ")
+# print(x+y)
+# print("subtracting : ")
+# print(x-y)
+# print("Multiplying : ")
+# print(x*y)
+# print("dividing : ")
+# print(x/y)
+
+# Write a program to prompt a user to enter 3 numbers and print the greatest number of
+# these 3 numbers
+x = int(input())
+y = int(input())
+z = int(input())
+print(max(x,max(y,z)))
